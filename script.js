87c096b3dde129902bca08e8d1c72604729e0aee
suleiman gacheru <dretrevor8@gmail.com>
2023-06-20 00:22:08 +0300

    make changes on menu spacing and display flow

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const hamburgerBtn = document.querySelector('.hamburgerBtn');
+
+
+  const hamburgerBtn = document.querySelector('.hamburgerBtn');
 const menu = document.querySelector('.mobile-nav');
 const closeMenu = document.querySelector('.x-icon');
+const menuOptions = menu.getElementsByTagName('li');
 hamburgerBtn.addEventListener('click', () => {
   menu.classList.remove('hidden');
   menu.classList.toggle('visible');
@@ -10,3 +13,10 @@ closeMenu.addEventListener('click', () => {
   menu.classList.remove('visible');
   menu.classList.toggle('hidden');
 });
+
+for (let i = 0; i < menuOptions.length; i += 1) {
+  menuOptions[i].addEventListener('click', () => {
+    menu.classList.remove('visible');
+    menu.classList.toggle('hidden');
+  });
+}
