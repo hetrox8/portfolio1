2568ce0f90621c85095bfbe7a1eb918066e1a1e2
ajroudirida <hpwin10kamilio@gmail.com>
2023-06-19 16:58:32 +0100

    add javascript for the menu

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
-const hamburgerBtn = document.querySelector(".hamburgerBtn");
-const menu = document.querySelector(".mobile-nav");
-const close_menu = document.querySelector("x-icon");
-console.log(menu.style.visibility);
-hamburgerBtn.addEventListener("click", function(){
-  menu.classList.remove("hidden");
-  menu.classList.toggle("visible");
-  // menu.style.display = "block";
-  console.log(menu.style);
-
+const hamburgerBtn = document.querySelector('.hamburgerBtn');
+const menu = document.querySelector('.mobile-nav');
+const closeMenu = document.querySelector('.x-icon');
+hamburgerBtn.addEventListener('click', () => {
+  menu.classList.remove('hidden');
+  menu.classList.toggle('visible');
 });
 
-close_menu.addEventListener("click", function(){
-  // if (menu.style.display == "block")
-  // {
-  //   menu.style.display = "none";
-   
-  // }
-
-    menu.classList.remove("visible");
-    menu.classList.toggle("hidden");
-    
-  
+closeMenu.addEventListener('click', () => {
+  menu.classList.remove('visible');
+  menu.classList.toggle('hidden');
 });
