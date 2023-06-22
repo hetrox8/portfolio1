aeecad21649fc983d4a43b7629d49e3e7c3c2088
ajroudirida <hpwin10kamilio@gmail.com>
2023-06-22 13:38:12 +0100

    add modal with js

--- pop.js
+++ pop.js
@@ -1,1 +1,39 @@
 
+const workContainer = document.querySelector('.works');
+const projects = workContainer.getElementsByClassName('project');
+const modal = document.querySelector('.modal-container');
+
+for (let i = 0; i < projects.length; i += 1) {
+  const projectBtn = projects[i].getElementsByClassName('btn');
+  console.log(projects[i]);
+  projectBtn[0].addEventListener('click', () => {
+    modal.classList.remove('hiddenModal');
+    modal.classList.toggle('showModal');
+    document.querySelector('.bg-blur').style.display = 'block';
+    modal.getElementsByTagName('h3')[0].textContent = projects[i].getElementsByTagName('h3')[0].textContent;
+    modal.getElementsByClassName('project-img')[0].setAttribute('src', projects[i].getElementsByClassName('first-image')[0].getAttribute('src'));
+    modal.getElementsByClassName('description')[0].textContent = projects[i].getElementsByTagName('p')[0].textContent;
+    const projectStack = projects[i].getElementsByClassName('item-2 ani')[0];
+
+    const modalProjectStack = modal.getElementsByClassName('project-tech-stack')[0];
+
+    modalProjectStack.innerHTML = '';
+
+    for (let i = 0; i < projectStack.getElementsByTagName('li').length; i += 1) {
+      const content = projectStack.getElementsByTagName('li')[i].textContent;
+
+      const a = document.createElement('a');
+      a.textContent = content;
+      const li = document.createElement('li');
+      li.appendChild(a);
+
+      modalProjectStack.appendChild(li);
+    }
+
+    modal.getElementsByClassName('close-modal')[0].addEventListener('click', () => {
+      modal.classList.remove('showModal');
+      modal.classList.toggle('hiddenModal');
+      document.querySelector('.bg-blur').style.display = 'none';
+    });
+  });
+}
