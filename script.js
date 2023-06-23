e3d616e47eec2133401035b3ef4fcb41ce02ca2a
suleiman gacheru <dretrevor8@gmail.com>
2023-06-23 14:43:38 +0300

    validate  form

--- script.js
+++ script.js
@@ -20,3 +20,20 @@ for (let i = 0; i < menuOptions.length; i += 1) {
     menu.classList.toggle('hidden');
   });
 }
+
+// validation contact form
+const form = document.getElementById('first-form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = document.querySelector('.email_input').value;
+  const suggestedEmail = document.querySelector('.email_input').value.toLowerCase();
+
+  if (email === suggestedEmail) {
+    form.submit();
+  } else {
+    const errorMessage = document.querySelector('.error');
+    errorMessage.textContent = `Your form is not sent, please enter your email in lowercase, e.g. ${suggestedEmail}`;
+    errorMessage.style.display = 'block';
+  }
+});
